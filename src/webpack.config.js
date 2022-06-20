888207d6b681a22877095f02f45538408c40d527
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-06-21 08:52:33 +0900

    add: 読み込むファイルの形式にtsを追加した

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.tsx$/,
+                test: /\.(ts|tsx)$/,
                 use: 'ts-loader',
             },
         ],
