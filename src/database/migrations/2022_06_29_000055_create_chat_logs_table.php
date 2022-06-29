2dd16db36eb9aab7e4ea3fbaa3c7b3489808e351
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-06-29 09:19:27 +0900

    fix: chat_logsにchat_idを追加した

--- src/database/migrations/2022_06_29_000055_create_chat_logs_table.php
+++ src/database/migrations/2022_06_29_000055_create_chat_logs_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     {
         Schema::create('chat_logs', static function (Blueprint $table) {
             $table->id();
+            $table->foreignId('chat_id')->constrained();
             $table->foreignId('user_id')->constrained();
             $table->string('comment_type');
             $table->timestamps();
