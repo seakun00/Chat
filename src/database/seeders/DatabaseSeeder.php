5feb379f1be880fb379a2d2f3d087566be549a3d
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-06-30 09:27:53 +0900

    add: commentsにテストデータを追加した

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             ChatSeeder::class,
             ChatAdminSeeder::class,
+            CommentSeeder::class,
         ]);
     }
 }
