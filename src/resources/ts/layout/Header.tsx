a1b473ce117c5d2a7ad73a1b037e77ccc98f9223
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-07-15 21:27:35 +0900

    add: ログインフォーム以外でヘッダーにログアウトボタンが表示されるようにした

--- src/resources/ts/layout/Header.tsx
+++ src/resources/ts/layout/Header.tsx
@@ -6,16 +6,25 @@ import {
     Typography
 } from "@mui/material";
 import React from "react";
+import { useLocation } from "react-router-dom";
 
-export const Header = () => (
-    <AppBar position="relative">
-        <Container>
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Chat App
-                </Typography>
-                <Button color="inherit">Login</Button>
-            </Toolbar>
-        </Container>
-    </AppBar>
-);
+export const Header = () => {
+    const location = useLocation();
+    console.log(location);
+
+    // 現状ログインフォーム以外は認証が必要なので、パスを見てログアウトボタンを表示するか判定する。
+    return (
+        <AppBar position="relative">
+            <Container>
+                <Toolbar>
+                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
+                        Chat App
+                    </Typography>
+                    {location.pathname !== '/login' &&
+                        <Button color="inherit" href="/logout">ログアウト</Button>
+                    }
+                </Toolbar>
+            </Container>
+        </AppBar>
+    )
+};
