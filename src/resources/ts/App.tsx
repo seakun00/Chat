46d2bb89d731d002577be688eec8b9a07c8a818e
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-07-15 21:46:25 +0900

    add: チャットページのルーティングを追加した

--- src/resources/ts/App.tsx
+++ src/resources/ts/App.tsx
@@ -4,12 +4,14 @@ import { Login } from "@/ts/Login";
 import { Chats } from "@/ts/chat/Chats";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Route } from "@/ts/layout/Route";
+import { Chat } from "@/ts/chat/Chat";
 
 export const App = () => (
     <QueryClientProvider client={new QueryClient()}>
         <BrowserRouter>
             <Route path="login" element={<Login/>}/>
             <Route path="chats" element={<Chats/>}/>
+            <Route path="chats/:id" element={<Chat/>}/>
         </BrowserRouter>
     </QueryClientProvider>
 );
---
+++ src/resources/ts/chat/Chat.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Chat = () => (
+    <h1>チャット</h1>
+);
