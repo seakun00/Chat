476e5f7ae145ab513809a17332b8429a09b0ee37
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-07-16 21:56:24 +0900

    add: テスト用のテキストコメントを追加する処理をシーダーに追加した

--- src/app/Models/Comment.php
+++ src/app/Models/Comment.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Comment extends Model
 {
+    use HasFactory;
+
     protected $casts = [
         'id' => 'int',
         'chat_id' => 'int',
--- src/database/seeders/TextSeeder.php
+++ src/database/seeders/TextSeeder.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Enums\CommentType;
+use App\Models\Chat;
 use App\Models\Comment;
 use App\Models\Text;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class TextSeeder extends Seeder
@@ -17,5 +20,22 @@ class TextSeeder extends Seeder
             'comment_id' => Comment::findOrFail(1)->id,
             'text' => '玉ねぎで簡単に作れる料理ありませんか？',
         ]);
+        $this->createTextComment(100);
+    }
+
+    private function createTextComment(int $count): void
+    {
+        for ($i = 0; $i < $count; $i++) {
+            $comment = Comment::factory()
+                ->create([
+                    'chat_id' => Chat::findOrFail(1)->id,
+                    'user_id' => User::findOrFail(1)->id,
+                    'type' => CommentType::TEXT,
+                ]);
+            Text::factory()
+                ->create([
+                    'comment_id' => $comment->id,
+                ]);
+        }
     }
 }
