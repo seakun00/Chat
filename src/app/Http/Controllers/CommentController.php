73c74e3e5e348b80ff9a47e568dcf0edf9becb1e
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-07-17 23:40:59 +0900

    change: コメント一覧取得APIのoffsetとlimitを必須にした

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CommentIndexFormRequest;
 use App\Models\Chat;
 use App\Repositories\CommentRepository;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
@@ -15,19 +15,15 @@ class CommentController extends Controller
     {
     }
 
-    public function index(Request $request, Chat $chat): JsonResponse
+    public function index(CommentIndexFormRequest $request, Chat $chat): JsonResponse
     {
         $query = $this->commentRepository->getByChatAsBuilder($chat);
+
         $count = $query->count();
+        $comments = $query->offset($request->get('offset'))
+            ->limit($request->get('limit'))
+            ->get();
 
-        if ($request->has('offset')) {
-            $query->offset($request->get('offset'));
-        }
-        if ($request->has('limit')) {
-            $query->limit($request->get('limit'));
-        }
-
-        $comments = $query->get();
         return response()->json([
             'comments' => $comments,
             'count' => $count,
