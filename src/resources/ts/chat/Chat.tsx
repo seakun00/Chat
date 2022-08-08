2e8f22c7eb0cf84d3f9d17edc1a8be1f4a62e0d5
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-08-09 00:10:45 +0900

    add: コメント一覧を追加した

--- src/resources/ts/chat/Chat.tsx
+++ src/resources/ts/chat/Chat.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { Chat as ChatType, getChat } from "@/ts/http/chat";
+import { useParams } from "react-router-dom";
+import { Stack } from "@mui/material";
+import { Comments } from "@/ts/chat/Comments";
 
-export const Chat = () => (
-    <h1>チャット</h1>
-);
+export const Chat = () => {
+    const { id: chatId } = useParams<{ id: string }>()
+    const chat = useQuery<ChatType, Error>(
+        ['chat', chatId],
+        () => {
+            return getChat(Number(chatId))
+        }
+    );
+
+    return (
+        <Stack
+            spacing={2}
+            sx={theme => ({
+                height: `calc(100% - ${theme.mixins.toolbar.minHeight}px)`
+            })}
+        >
+            <Comments />
+        </Stack>
+    )
+};
---
+++ src/resources/ts/chat/Comments.tsx
@@ -0,0 +1,51 @@
+import { List, ListItem, ListItemText } from "@mui/material";
+import React, {UIEvent, useEffect, useRef, useState} from "react";
+import { useParams } from "react-router-dom";
+import { useGetComments } from "@/ts/chat/useGetComments";
+
+export const Comments = () => {
+    const { id: chatId } = useParams<{ id: string }>()
+    const [offset, setOffset] = useState(0);
+    const comments = useGetComments(Number(chatId), 30, offset);
+
+    const commentList = useRef<HTMLUListElement>(null);
+    const [scrollPosition, setScrollPosition] = useState<HTMLLIElement|null>(null);
+    useEffect(() => {
+        scrollPosition
+            ? scrollPosition.scrollIntoView()
+            : commentList.current?.scroll(0, commentList.current.scrollHeight)
+    }, [comments]);
+
+    const handleScroll = (event: UIEvent<HTMLUListElement>) => {
+        const ul = event.currentTarget;
+        const isTop = ul.scrollTop === 0;
+        const isBottom = ul.scrollTop + ul.offsetHeight === ul.scrollHeight;
+
+        if (isTop) {
+            // コメント一覧更新時にスクロール位置がトップに移動しないように、更新前のトップの位置を保存する。
+            commentList.current && setScrollPosition(commentList.current.firstElementChild as HTMLLIElement)
+
+            // コメント追加のタイミングと被ってoffsetが少し前にずれることもあるけど、重複削除してチャットに追加するため問題ない。
+            setOffset(comments.length)
+        } else if (isBottom) {
+            // TODO: コメント追加やサーバープッシュの実装時に追加する
+        }
+    }
+
+    return (
+        <List
+            ref={commentList}
+            onScroll={handleScroll}
+            sx={{
+                height: '100%',
+                overflow: 'auto'
+            }}
+        >
+            {comments.map((comment) => (
+                <ListItem key={comment.id}>
+                    <ListItemText primary={comment.text} />
+                </ListItem>
+            ))}
+        </List>
+    )
+}
