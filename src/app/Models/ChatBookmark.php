e97e833c15e9eb27fb10ffd71c48d9fc04d4043e
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-08-15 22:42:32 +0900

    add: ChatBookmarkを追加した

---
+++ src/app/Models/ChatBookmark.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class ChatBookmark extends Model
+{
+    use HasFactory;
+
+    protected $casts = [
+        'id' => 'int',
+        'chat_id' => 'int',
+        'user_id' => 'int',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function chat(): BelongsTo
+    {
+        return $this->belongsTo(Chat::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
