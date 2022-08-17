61e6d8f265898f3ba26e6eb69da71563818d7b8b
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-08-18 01:51:47 +0900

    refactor: ChatControllerのindexにフォームリクエストを追加した

--- src/app/Http/Controllers/ChatController.php
+++ src/app/Http/Controllers/ChatController.php
@@ -4,26 +4,25 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ChatIndexRequest;
 use App\Models\Chat;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    public function index(Request $request): JsonResponse
+    public function index(ChatIndexRequest $request): JsonResponse
     {
-        // TODO: offsetとlimitをFormRequestで検証する
         $query = Chat::query();
+
         $count = $query->count();
 
-        if ($request->has('offset')) {
-            $query->offset($request->get('offset'));
-        }
-        if ($request->has('limit')) {
-            $query->limit($request->get('limit'));
-        }
+        $offset = $request->get('offset');
+        $limit = $request->get('limit');
+        $chats = $query->offset($offset)
+            ->limit($limit)
+            ->get();
 
-        $chats = $query->get();
         return response()->json([
             'chats' => $chats,
             'count' => $count,
