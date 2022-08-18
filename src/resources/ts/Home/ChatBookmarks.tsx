af8248ad7360a8ebc27f52eedbccc898ee9d22e0
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-08-18 23:52:19 +0900

    add: チャットのブックマーク一覧コンポーネントを追加した

--- src/resources/ts/Home/ChatBookmarks.tsx
+++ src/resources/ts/Home/ChatBookmarks.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { MouseEvent } from "react";
+import { useQuery } from "react-query";
+import { ChatBookmark, getChatBookmarks } from "@/ts/http/chatBookmarks";
+import { Loading } from "@/ts/layout/Loading";
+import {
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
+} from "@mui/material";import {ErrorAlert} from "@/ts/layout/Error";
 
 export const ChatBookmarks = () => {
-    return (
-        <h1>ブックマーク一覧</h1>
-    )
+    const { isLoading, data } = useQuery<ChatBookmark[], Error>('chat_bookmarks', getChatBookmarks);
+
+    const handleSelectChatBookmark = (chatId: number) => {
+        // コメント一覧のチャットIDを設定する
+    }
+
+    if (isLoading) {
+        return <Loading />;
+    } else if (data) {
+        return (
+            <List>
+                {data.map((chatBookmark) => (
+                    <ListItem disablePadding key={chatBookmark.id}>
+                        <ListItemButton onClick={() => handleSelectChatBookmark(chatBookmark.chat_id)}>
+                            <ListItemText primary={chatBookmark.name} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        )
+    } else {
+        return <ErrorAlert />;
+    }
 }
