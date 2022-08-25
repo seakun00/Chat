05bfadb3b32c609e9bc5425279dc9d5335b6e2d1
Kai Koyamatsu <k.koyamatsu@cartahd.com>
2022-08-26 01:26:59 +0900

    refactor: 不要なreturnを削除した

--- src/resources/ts/Home/Home.tsx
+++ src/resources/ts/Home/Home.tsx
@@ -4,17 +4,15 @@ import { ChatBookmarks } from "@/ts/Home/ChatBookmarks";
 import { ChatIdProvider } from "@/ts/Home/ChatIdProvider";
 import { Chat } from "@/ts/Home/Chat";
 
-export const Home = () => {
-    return (
-        <ChatIdProvider>
-            <Grid container spacing={2}>
-                <Grid item xs={4}>
-                    <ChatBookmarks />
-                </Grid>
-                <Grid item xs={8}>
-                    <Chat />
-                </Grid>
+export const Home = () => (
+    <ChatIdProvider>
+        <Grid container spacing={2}>
+            <Grid item xs={4}>
+                <ChatBookmarks />
             </Grid>
-        </ChatIdProvider>
-    );
-}
+            <Grid item xs={8}>
+                <Chat />
+            </Grid>
+        </Grid>
+    </ChatIdProvider>
+);
