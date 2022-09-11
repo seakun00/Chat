acd9eca9e29be27ff11e805e66c3efd76a6a4202
seakun00 <seakun00@gmail.com>
2022-09-12 07:30:49 +0900

    change: タイトルを変更した

--- src/resources/ts/layout/Header.tsx
+++ src/resources/ts/layout/Header.tsx
@@ -17,7 +17,7 @@ export const Header = () => {
             <Container maxWidth={false}>
                 <Toolbar>
                     <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        Chat App
+                        オープンチャット
                     </Typography>
                     {location.pathname !== '/login' &&
                         <Button color="inherit" href="/logout">ログアウト</Button>
