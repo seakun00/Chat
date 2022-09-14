4d8ea7fd6464aab3f82988fa8f3fd364093adf4e
seakun00 <seakun00@gmail.com>
2022-09-15 00:46:50 +0900

    refactor: Containerが不要になったので削除した

--- src/resources/ts/layout/HomeRoute.tsx
+++ src/resources/ts/layout/HomeRoute.tsx
@@ -5,66 +5,57 @@ import {
 } from "react-router-dom";
 import React, {ReactNode} from "react";
 import { Header, Spacer } from "@/ts/layout/Header";
-import {Container, CssBaseline, Grid} from "@mui/material";
+import {CssBaseline, Grid} from "@mui/material";
 import { useAppBarHeight } from "@/ts/layout/useAppBarHeight";
 import {ChatIdProvider} from "@/ts/Home/ChatIdProvider";
 import {main} from "@/ts/layout/color";
 import {ChatBookmarks} from "@/ts/Home/ChatBookmarks";
 
-export const HomeRoute = (props: RouteProps) => {
+export const HomeRoute = (props: RouteProps) => (
+    <Routes>
+        <BaseRoute
+            {...props}
+            element={
+                <>
+                    <CssBaseline/>
+                    <Header/>
+                    <Spacer/>
+                    <Home>{props.element}</Home>
+                </>
+            }
+        />
+    </Routes>
+);
+
+const Home = (props: {children: ReactNode}) => {
     // https://github.com/mui/material-ui/issues/10739
     const appBarMinHeight = useAppBarHeight();
 
-    return (
-        <Routes>
-            <BaseRoute
-                {...props}
-                element={
-                    <>
-                        <CssBaseline/>
-                        <Header/>
-                        <Spacer/>
-                        <Container
-                            component="main"
-                            maxWidth={false}
-                            disableGutters
-                            sx={{
-                                height: `calc(100vh - ${appBarMinHeight}px)`,
-                            }}
-                        >
-                            <Home>{props.element}</Home>
-                        </Container>
-                    </>
-                }
-            />
-        </Routes>
-    );
-};
+    // Grid containerでmargin topに-16pxが設定されるので調整してる
+    // https://stackoverflow.com/questions/50416346/grid-with-negative-margin-top-break-my-ui
+    const fixGridContainerMarginTopPx = '16';
 
-const Home = (props: {children: ReactNode}) => (
-    <ChatIdProvider>
-        <Grid
-            container
-            spacing={2}
-            sx={{
-                // Grid containerでmargin topに-16pxが設定されるので調整してる
-                // https://stackoverflow.com/questions/50416346/grid-with-negative-margin-top-break-my-ui
-                height: `calc(100% + 16px)`,
-            }}
-        >
+    return (
+        <ChatIdProvider>
             <Grid
-                item
-                xs={2}
-                sx={{
-                    backgroundColor: main,
-                    color: "white",
-                }}
+                container
+                spacing={2}
+                sx={{ height: `calc(100vh - ${appBarMinHeight}px + ${fixGridContainerMarginTopPx}px)` }}
             >
-                <ChatBookmarks/>
+                <Grid
+                    item
+                    xs={2}
+                    sx={{
+                        backgroundColor: main,
+                        color: "white",
+                    }}
+                >
+                    <ChatBookmarks/>
+                </Grid>
+                <Grid item xs={10}>
+                    {props.children}
+                </Grid>
             </Grid>
-            <Grid item xs={10}>
-                {props.children}
-            </Grid>
-        </Grid>
-    </ChatIdProvider>
-);
+        </ChatIdProvider>
+    );
+}
