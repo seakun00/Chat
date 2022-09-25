e28f0edb018f4a1a434a8bb103d1ce803902388e
seakun00 <seakun00@gmail.com>
2022-09-25 18:22:46 +0900

    add: フロントエンドの不要なインポートを削除するプラグインを追加した

--- src/resources/ts/layout/HomeRoute.tsx
+++ src/resources/ts/layout/HomeRoute.tsx
@@ -1,11 +1,7 @@
 import { Routes, Route as BaseRoute, RouteProps } from 'react-router-dom';
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { Header, Spacer } from '@/ts/layout/Header';
-import { CssBaseline, Grid } from '@mui/material';
-import { useAppBarHeight } from '@/ts/layout/useAppBarHeight';
-import { ChatIdProvider } from '@/ts/Home/ChatIdProvider';
-import { main } from '@/ts/layout/color';
-import { ChatBookmarks } from '@/ts/Home/ChatBookmarks';
+import { CssBaseline } from '@mui/material';
 import { Home } from '@/ts/Home/Home';
 
 export const HomeRoute = (props: RouteProps) => (
