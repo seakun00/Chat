519cbf69e0d8c7c8cc8dd88930d1e1340c148f81
seakun00 <seakun00@gmail.com>
2022-10-05 22:09:40 +0900

    refactor: 不要な処理を削除した

--- src/resources/ts/Chat/Chat.tsx
+++ src/resources/ts/Chat/Chat.tsx
@@ -1,6 +1,4 @@
 import React, { useContext } from 'react';
-// import { useQuery } from 'react-query';
-// import { Chat as ChatType, getChat } from '@/ts/http/chat';
 import { Stack } from '@mui/material';
 import { Comments } from '@/ts/Chat/Comments';
 import { ChatIdContext } from '@/ts/Home/ChatIdProvider';
@@ -10,15 +8,11 @@ export const Chat = () => {
     const { chatId } = useContext(ChatIdContext);
     if (chatId === undefined) {
         return <ChatsLink />;
+    } else {
+        return (
+            <Stack spacing={2} sx={{ height: '100%' }}>
+                <Comments />
+            </Stack>
+        );
     }
-
-    // const chat = useQuery<ChatType, Error>(['chat', chatId], () => {
-    //     return getChat(Number(chatId));
-    // });
-
-    return (
-        <Stack spacing={2} sx={{ height: '100%' }}>
-            <Comments />
-        </Stack>
-    );
 };
